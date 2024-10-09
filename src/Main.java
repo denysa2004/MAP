c2bb8e15294ea0d6d220ac399f2ffb9a7e1ac96d
denysa2004 <149471938+denysa2004@users.noreply.github.com>
2024-10-09 16:25:27 +0300

    Aufgabe 1 added

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,34 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+         Universitat universitat = new Universitat();
+         int[] noten={29,37,38,41,84,67};
+         //ausreicehende Noten
+         int[] nichtAusreichendeNoten=universitat.ausreichend(noten);
+        System.out.print('[');
+        for (int i = 0; i < nichtAusreichendeNoten.length; i++) {
+            if(i!=nichtAusreichendeNoten.length-1)
+                System.out.print(nichtAusreichendeNoten[i] + ", ");
+            else
+                System.out.print(nichtAusreichendeNoten[i]);
 
         }
+        System.out.println(']');
+        //durchschnittsnote
+         System.out.println(universitat.durchschnittsNote(noten));
+
+         //abgerundete Noten
+         int[] abgerundeteNoten=universitat.abgerundeteNoten(noten);
+        System.out.print("[");
+        for(int i=0;i<abgerundeteNoten.length;i++){
+            if(i!=abgerundeteNoten.length-1)
+                System.out.print(abgerundeteNoten[i]+",");
+            else
+                System.out.print(abgerundeteNoten[i]);
+        }
+        System.out.println(']');
+
+        //maximale abgerundete Note
+         System.out.println(universitat.maximaleAbgerundeteNoten());
+        }
     }
-}
--- src/Universitat.java
+++ src/Universitat.java
@@ -1,3 +1,60 @@
 public class Universitat {
 
+    public int[] nichtAusreichendeNoten;
+    public int[] abgerundeteNoten;
+
+    public int[] ausreichend(int[] noten) {
+        int nichtAusreichendeNotenZahl = 0;
+        for (int i = 0; i < noten.length; i++) {
+            if (noten[i] < 40) {
+                nichtAusreichendeNotenZahl++;
+            }
+        }
+        nichtAusreichendeNoten = new int[nichtAusreichendeNotenZahl];
+
+        int index = 0;
+        for (int i = 0; i < noten.length; i++) {
+            if (noten[i] < 40) {
+                nichtAusreichendeNoten[index] = noten[i];
+                index++;
+            }
+        }
+        return nichtAusreichendeNoten;
+    }
+
+    public double durchschnittsNote(int[] noten) {
+        float sum = 0;
+        for (int i = 0; i < noten.length; i++) {
+            sum += noten[i];
+        }
+        double average = sum / noten.length;
+        return (Math.round(average * 100) / 100.00);
+    }
+
+    public int[] abgerundeteNoten(int[] noten) {
+        int notenZahl=noten.length;
+        abgerundeteNoten = new int[notenZahl];
+
+        for (int i = 0; i < noten.length; i++) {
+            int rest = noten[i] % 5;
+            if (noten[i] >= 38 && (noten[i] + 5 - rest) - noten[i] < 3)
+                    abgerundeteNoten[i]=noten[i] + 5 - rest;
+                else
+                    abgerundeteNoten[i]=noten[i] ;
+
+        }
+        return abgerundeteNoten;
+    }
+
+
+    public int maximaleAbgerundeteNoten(){
+        int maxim=0;
+        for(int i=0;i<abgerundeteNoten.length;i++){
+            if(abgerundeteNoten[i]>maxim)
+                maxim=abgerundeteNoten[i];
+        }
+        return maxim;
+    }
 }
+
+
